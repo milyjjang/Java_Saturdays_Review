d2da94a9ff3d0d95e950c806968dd0ab86718c1f
Emily Yang <em.ek.yang@gmail.com>
2022-12-10 12:39:42 -0500

    Teacher subclass created

---
+++ src/week11_Review/Teacher.java
@@ -0,0 +1,8 @@
+package week11_Review;
+
+public class Teacher extends Employee{
+
+    public Teacher(String name, int age, char gender, String employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+}
