009a3a41ac7e95caba04428e39606282c59b6061
Emily Yang <em.ek.yang@gmail.com>
2022-12-17 10:31:47 -0500

    created parent class "shape"

---
+++ src/week12_Review/Shape.java
@@ -0,0 +1,60 @@
+package week12_Review;
+
+public abstract class Shape {
+
+    private final String name;
+
+    public Shape(String name) {
+        this.name = getClass().getSimpleName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected abstract void calc_area();
+    protected abstract void calc_perimeter();
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                "area='" + name + '\'' +
+                "perimeter='" + name + '\'' +
+                '}';
+    }
+}
+
+
+/*
+Shape Task:
+    1. Create a class named Shape:
+            Variables:
+                name (final)
+
+            Encapsulate all the fields
+
+            Methods:
+                calc_area()
+                calc_perimeter()
+                toString(): include the area & perimeter of the shape
+
+    2. Create an interface named Volume:
+
+            Variables:
+                HAS_VOLUME
+
+            Methods:
+                calc_volume()
+
+
+    3. Create the following sub-classes of Shape and add any additional variables/methods that are needed:
+
+            3.1 Square
+            3.2 Rectangle
+            3.3 Circle
+            3.4 Cylinder
+            3.5 Cube
+
+    4. Implement the Volume interface to Cylinder and Cube classes
+ */
