8f09083595b8ae95d89da3481d0c1812eeba9dd0
Emily Yang <em.ek.yang@gmail.com>
2022-12-17 10:50:18 -0500

    created child class "rectangle"

---
+++ src/week12_Review/Rectangle.java
@@ -0,0 +1,46 @@
+package week12_Review;
+
+public class Rectangle extends Shape {
+
+    private double length, width;
+
+    public Rectangle(String name, double length, double width) {
+        super(name);
+        setLength(length);
+        setWidth(width);
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    protected double calc_area() {
+        return length * width;
+    }
+
+    @Override
+    protected double calc_perimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}", "") +
+                "length=" + length +
+                ", width=" + width +
+                '}';
+    }
+}
